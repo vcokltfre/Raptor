819b43cfa8b79902985cf9a7e7d279f3ebb98b68
vcokltfre <vcokltfre@gmail.com>
2021-09-09 00:15:25 +0100

    feat: add config provider util

---
+++ api/config.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+from os import environ
+from pathlib import Path
+
+from yaml import safe_load
+
+
+@lru_cache()
+def _get_local_config(location: str, guild_id: int) -> dict:
+    fp = Path(location) / f"{guild_id}.yml"
+
+    return safe_load(fp.read_text())
+
+
+async def get_guild_config(guild_id: int) -> dict:
+    provider = environ["CONFIG_PROVIDER"]
+
+    type, location = provider.split(":")
+
+    if type == "local":
+        return _get_local_config(location, guild_id)
+
+    raise ValueError("Invalid config provider set.")
