6b6f6eff768e14c4d7570cd00b0cacea6242eca6
vcokltfre <vcokltfre@gmail.com>
2021-09-17 06:36:53 +0100

    feat: add schemas init

---
+++ common/schemas/__init__.py
@@ -0,0 +1,15 @@
+from typing import Dict, Union, List
+
+from pydantic import BaseModel
+
+from .logging import LoggingModel
+
+
+class Plugins(BaseModel):
+    logging: LoggingModel
+
+
+class ConfigSchema(BaseModel):
+    prefix: Union[str, List[str]]
+    levels: Dict[int, int]
+    plugins: Plugins
